975511380b7fd9163fd11d488d4d5eb5775fc627
classic-creator <himangshukhound322@gmail.com>
2023-03-21 22:00:06 +0530

    21 march

--- src/componant/user/LoginRegister.js
+++ src/componant/user/LoginRegister.js
@@ -28,65 +28,34 @@ const LoginRegister = () => {
     return (
         <Fragment>
             {/* Register form */}
-            <div className="formContainer container">
-                <div className="title">
+            <div className="formContainer ">
+                <div className="content">
+               
                     <div>
-                        <div>
+                        <div className='login_signUp_toggle'>
                             <p onClick={(e) => switchTabs(e, "login")} >Login</p>
                             <p onClick={(e) => switchTabs(e, "register")}>Register</p>
                         </div>
                         <button ref={switcherTab}></button>
-                    </div>
+                    
 
                 </div >
-                <div className="content">
-                    <form action="#" ref={registerTab}>
+                    <form action="#" className='loginForm' ref={loginTab}>
                         <div className="user-details">
-                            <div className="input-box">
-                                <span className="details">Full Name</span>
-                                <input type="text" placeholder="Enter your name" required />
-                            </div>
-                            <div className="input-box">
-                                <span className="details">Username</span>
-                                <input type="text" placeholder="Enter your username" required />
-                            </div>
+                            
+                           
                             <div className="input-box">
                                 <span className="details">Email</span>
                                 <input type="text" placeholder="Enter your email" required />
                             </div>
-                            <div className="input-box">
-                                <span className="details">Phone Number</span>
-                                <input type="text" placeholder="Enter your number" required />
-                            </div>
+                            
                             <div className="input-box">
                                 <span className="details">Password</span>
                                 <input type="text" placeholder="Enter your password" required />
                             </div>
-                            <div className="input-box">
-                                <span className="details">Confirm Password</span>
-                                <input type="text" placeholder="Confirm your password" required />
-                            </div>
+                            
                         </div>
-                        <div className="gender-details">
-                            <input type="radio" name="gender" id="dot-1" />
-                            <input type="radio" name="gender" id="dot-2" />
-                            <input type="radio" name="gender" id="dot-3" />
-                            <span className="gender-title">Gender</span>
-                            <div className="category">
-                                <label for="dot-1">
-                                    <span className="dot one"></span>
-                                    <span className="gender">Male</span>
-                                </label>
-                                <label for="dot-2">
-                                    <span className="dot two"></span>
-                                    <span className="gender">Female</span>
-                                </label>
-                                <label for="dot-3">
-                                    <span className="dot three"></span>
-                                    <span className="gender">Prefer not to say</span>
-                                </label>
-                            </div>
-                        </div>
+                        
                         <div className="button">
                             <input type="submit" value="Register" />
                         </div>
@@ -94,7 +63,7 @@ const LoginRegister = () => {
                 {/* </div> */}
                 {/* login form */}
                 {/* <div className="content"> */}
-                    <form action="#"  ref={loginTab}>
+                    <form action="#" className=' registerForm' ref={registerTab}>
                         <div className="user-details">
                             <div className="input-box">
                                 <span className="details">Full Name</span>
